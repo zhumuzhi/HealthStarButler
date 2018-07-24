ecd28daa642bc229a9af136f391f813db6300434
ZMZ <zhumuzhi@icloud.com>
2018-07-24 23:05:58 +0800

    1.调整

---
+++ HealthStarButler/Classes/Consultation (咨询)/SubController/FSMine(我的方盛)/SubController/FSAccountSetController/View/FSAccountSetHeadCell.m
@@ -0,0 +1,41 @@
+//
+//  FSAccountSetHeadCell.m
+//  HealthStarButler
+//
+//  Created by 朱慕之 on 2018/7/24.
+//  Copyright © 2018年 zhumuzhi. All rights reserved.
+//
+
+#import "FSAccountSetHeadCell.h"
+
+@interface FSAccountSetHeadCell()
+
+
+
+
+@end
+
+@implementation FSAccountSetHeadCell
+
+#pragma mark - init
+- (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
+    if(self = [super initWithStyle:style reuseIdentifier:reuseIdentifier]) {
+        [self setupUI];
+        
+    }
+    return self;
+}
+
+#pragma mark - configUI
+- (void)setupUI {
+    
+}
+
+#pragma mark - SetData
+
+#pragma mark - Event
+
+#pragma mark - LazyGet
+
+
+@end
